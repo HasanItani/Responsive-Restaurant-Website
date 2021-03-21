a1f0689b164d711924045091f788fffff145f81d
aliyassine26 <ali.yassine26@gmail.com>
2021-03-22 01:01:41 +0200

    signin signup

--- webpage/login.php
+++ webpage/login.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <?php 
 $message="";
-if(isset($_POST["submit"])){
+$message2="";
+$file = "../data/form.txt";
+
+
+if(isset($_POST["submitup"]) || isset($_POST["submitin"]) ){
+    
 // Sign UP
 if(strcmp($_POST["hidden"],"signup")==0){
-
+  
 $email=$_POST["emailup"];
 $password=$_POST["passwordup"];
 $repassword=$_POST["repasswordup"];
 
 if(strcmp($password,$repassword)==0){
 // same passwords
-$file = "../data/form.txt";
+
 $registered = FALSE;
 
 if (count(file($file)) != 0){
@@ -29,7 +34,8 @@ if ($registered){
 
 }
 else{
-    file_put_contents($file,$email.", ".$password."\n" ,FILE_APPEND);
+    $put = "$email, $password, \n";
+    file_put_contents($file,$put,FILE_APPEND);
     $message="DONE!";
 }
 }
@@ -45,14 +51,47 @@ $message="Passwords does not match ";
 }
 
 // Sign In
-else{
-// 
-    $username=$_POST["usernamein"];
-    $password=$_POST["passwordin"];
-  
-}
-}
+elseif(strcmp($_POST["hidden"],"signin")==0){
+
+    
+    $email = $_POST["emailin"];
+    $password = $_POST["passwordin"];
+    $registered = FALSE;
+    
+    
+    if (count(file($file)) != 0){
+        foreach (file($file) as $line) {  
+            list($emailtemp,$pass) = explode(", ",$line);
+           
+            if (strcmp($emailtemp, $email)==0 && strcmp($pass, $password)==0){
+       
+                $registered = TRUE;
+               
+                break;
+            }
+        }
+    }
+
+    if ($registered){
+		
+					$message2="Welcome Back!";
+                    header("Location: homepage.php");
+				
+				}
+                
+                else{
+		
+				$message2="Incorrect Username or Password";
+						
+		
+			
+				}
+    }
+
+
+            }
 ?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -64,19 +103,19 @@ else{
 <body>
 <div class="login-wrap">
 	<div class="login-html">
-		<input id="tab-1" type="radio" name="tab" class="sign-in" ><label for="tab-1" class="tab">Sign In</label>
-		<input id="tab-2" type="radio" name="tab" class="sign-up" checked ><label for="tab-2" class="tab">Sign Up</label>
+		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
+		<input id="tab-2" type="radio" name="tab" class="sign-up"  ><label for="tab-2" class="tab">Sign Up</label>
 		<div class="login-form">
 
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
 			<div class="sign-in-htm">
 				<div class="group">
 					<label for="user" class="label">Email</label>
-					<input id="user" name="emailin" type="text" class="input">
+					<input id="user" name="emailin" type="text" class="input" >
 				</div>
 				<div class="group">
 					<label for="pass" class="label">Password</label>
-					<input id="pass" type="password" name="passwordin" class="input" data-type="password">
+					<input id="pass" type="password" name="passwordin" class="input" data-type="password" >
 				</div>
 				<div class="group">
 					<input id="check" type="checkbox" class="check" checked>
@@ -84,7 +123,8 @@ else{
 				</div>
                 <input type="hidden" name="hidden" value="signin">
                 <!-- ---------------------------------------------------- -->
-				<div class="group">
+				<p style="color:red"><?=$message2?></p>
+                <div class="group">
 					<input type="submit" class="button" name="submitin" value="Sign In">
 				</div>
 				<div class="hr"></div>
@@ -100,15 +140,15 @@ else{
 			<div class="sign-up-htm">
 				<div class="group">
 					<label for="user" class="label">Email</label>
-					<input id="user" type="text" name="emailup" class="input">
+					<input id="user" type="text" name="emailup" class="input"required>
 				</div>
 				<div class="group">
 					<label for="pass" class="label">Password</label>
-					<input id="pass" name="passwordup" type="password" class="input" data-type="password">
+					<input id="pass" name="passwordup" type="password" class="input" data-type="password" required>
 				</div>
 				<div class="group">
 					<label for="pass" class="label">Repeat Password</label>
-					<input id="pass" type="password" name="repasswordup" class="input" data-type="password">
+					<input id="pass" type="password" name="repasswordup" class="input" data-type="password" required>
 				</div>
                 <p style="color:red"><?=$message?></p>
                 <input type="hidden" name="hidden" value="signup">
@@ -120,7 +160,7 @@ else{
                 </form>
                 <!-- --------------------------------------------------------------- -->
 				<div class="group">
-					<input type="submit" name="submit" class="button" value="Sign Up">
+					<input type="submit" name="submitup" class="button" value="Sign Up">
 				</div>
 				<div class="hr"></div>
 				<div class="foot-lnk">
