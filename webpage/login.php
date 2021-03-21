b8db79e1ccd56a7822d983d6e4e3f9136537b337
aliyassine26 <ali.yassine26@gmail.com>
2021-03-21 21:46:47 +0200

    edit login

--- webpage/login.php
+++ webpage/login.php
@@ -13,45 +13,57 @@
 		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
 		<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
 		<div class="login-form">
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
 			<div class="sign-in-htm">
 				<div class="group">
-					<label for="user" class="label">Username</label>
-					<input id="user" type="text" class="input">
+					<label for="user" class="label">Email</label>
+					<input id="user" name="emailin" type="text" class="input">
 				</div>
 				<div class="group">
 					<label for="pass" class="label">Password</label>
-					<input id="pass" type="password" class="input" data-type="password">
+					<input id="pass" type="password" name="passwordin" class="input" data-type="password">
 				</div>
 				<div class="group">
 					<input id="check" type="checkbox" class="check" checked>
 					<label for="check"><span class="icon"></span> Keep me Signed in</label>
 				</div>
+                <input type="hidden" name="hiddenin">
                 <!-- ---------------------------------------------------- -->
 				<div class="group">
-					<input type="submit" class="button" value="Sign In">
+					<input type="submit" class="button" name="submitin" value="Sign In">
 				</div>
 				<div class="hr"></div>
 				<div class="foot-lnk">
 					<a href="#forgot">Forgot Password?</a>
 				</div>
 			</div>
+</form>        
+            
+            
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+            <!-- Signup -->
 			<div class="sign-up-htm">
 				<div class="group">
-					<label for="user" class="label">Username</label>
-					<input id="user" type="text" class="input">
+					<label for="user" class="label">Email</label>
+					<input id="user" type="text" name="emailup" class="input">
 				</div>
 				<div class="group">
 					<label for="pass" class="label">Password</label>
-					<input id="pass" type="password" class="input" data-type="password">
+					<input id="pass" name="passwordup" type="password" class="input" data-type="password">
 				</div>
 				<div class="group">
 					<label for="pass" class="label">Repeat Password</label>
-					<input id="pass" type="password" class="input" data-type="password">
+					<input id="pass" type="password" name="repasswordup" class="input" data-type="password">
 				</div>
-				<div class="group">
+
+                <input type="hidden" name="hiddenup">
+
+				<!-- <div class="group">
 					<label for="pass" class="label">Email Address</label>
 					<input id="pass" type="text" class="input">
-				</div>
+				</div> -->
+                </form>
                 <!-- --------------------------------------------------------------- -->
 				<div class="group">
 					<input type="submit" class="button" value="Sign Up">
